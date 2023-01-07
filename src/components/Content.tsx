f27860cf169e09597acba0a060629680fff95931
serj <sergei.hudakov@gmail.com>
2023-01-07 17:00:57 +0300

    create main page structure

---
+++ src/components/Content.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Content = () => {
+    return (
+        <main className="content">
+            <div>
+                <img
+                    src="https://phonoteka.org/uploads/posts/2021-06/1624306054_56-phonoteka_org-p-oboi-plyazh-krasivo-65.jpg"
+                    alt=""
+                />
+            </div>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>new post</div>
+            </div>
+            <div>post1 post2 post3</div>
+        </main>
+    )
+}
+
+export default Content
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div>
+                <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt=""/>
+            </div>
+        </header>
+    )
+}
+
+export default Header
